143b971544ef11d05de5023bb50136a99e435bd5
SQ-Raz <razukmamo@gmail.com>
2024-10-03 19:54:22 +0100

    A7

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,8 +2,9 @@
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use App\Models\CommunityLink;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,11 +13,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        // Eliminar los registros existentes en la tabla community_links
+        DB::delete('delete from community_links');
+        
+        // Crear 50 nuevos registros en la tabla community_links usando el factory
+        CommunityLink::factory(50)->create();
     }
 }
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,16 +1,24 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+        <h2 class="font-semibold text-2xl text-gray-800 dark:text-gray-200 leading-tight">
             {{ __('Community Contributions') }}
         </h2>
     </x-slot>
 
-    <div class="py-12">
+    <div class="py-12 bg-gray-50 dark:bg-gray-900">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+            @foreach ($links as $link)
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-lg hover:shadow-xl transition-shadow duration-300 sm:rounded-lg mb-6">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __("Here you will see the Community Links!") }}
+                    <li class="text-lg font-semibold hover:text-blue-600 transition-colors duration-300">
+                        {{$link->title}}
+                    </li>
+                    <small>Contributed by: {{$link->creator->name}} {{$link->updated_at->diffForHumans()}}</small>
                 </div>
+            </div>
+            @endforeach
+            <div class="mt-4">
+                {{$links->links()}}
             </div>
         </div>
     </div>
